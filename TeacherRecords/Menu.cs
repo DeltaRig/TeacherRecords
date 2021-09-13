44f353a11ad5d8e8f615c113efa340ca05efee02
DeltaRig <hitomikaleiko@gmail.com>
2021-09-13 18:19:14 -0300

    start menu

---
+++ TeacherRecords/Menu.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherRecords
+{
+    class Menu
+    {
+        private TeacherBiz teacherBiz;
+        public Menu() {
+            teacherBiz = new TeacherBiz(
+                new List<Teacher>{
+                    new Teacher(1L, "Jurema", "200", "Fisic"),
+                    new Teacher(2L, "Lucas", "200", "DataBase"),
+                    new Teacher(3L, "Jorge", "201", "Math"),
+                    new Teacher(4L, "Carlos", "202", "Literature"),
+                    new Teacher(5L, "Carlos", "302", "Quimica")
+                }
+            );
+        }
+        public void Start()
+        {
+            string option = "";
+            do {
+                
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nWellcome to a Teacher Records's Rainbow School");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("1. See all teachers \n" +
+                                  "2. Search teachers \n" + 
+                                  "0. exit");
+                option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("List of teachers: ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        printList(teacherBiz.GetAllTeachers());
+                        break;
+                    case "2":
+                        searchBy();
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Invalid option, try again");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+            } while (!option.Equals("0"));
+
+        }
+
+        private void printList(IEnumerable<Teacher> list)
+        {
+            if(list.Count() == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Not data found");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            foreach(Teacher t in list)
+            {
+                Console.WriteLine(t);
+            }
+        }
+
+        private void searchBy()
+        {
+            string answer = "";
+            Console.WriteLine("1. Search teachers by ID\n" +
+                             "2. Search teachers by name" +
+                             "3. Search teachers by class\n" +
+                             "4. Search teachers by section\n" +
+                             "0. Come back to menu");
+            answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "1":
+                    Console.WriteLine("What is the teacher ID that you want search?");
+                    answer = Console.ReadLine();
+                    long id = -1L;
+                    try
+                    {
+                        id = long.Parse(answer);
+                    }
+                    catch (IOException ex)
+                    {
+                        Console.WriteLine("You should write a numeric, " + ex);
+                    }
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("List of teachers: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    printList(teacherBiz.GetTeachersByID(id));
+                    break;
+                case "2":
+                    Console.WriteLine("What is the teacher name that you want search?");
+                    answer = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("List of teachers: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    printList(teacherBiz.GetTeachersByName(answer));
+                    break;
+                case "3":
+                    Console.WriteLine("What is the class's teacher that you want search?");
+                    answer = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("List of teachers: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    printList(teacherBiz.GetTeachersByClass(answer));
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Invalid option, try again");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ TeacherRecords/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Menu menu = new Menu();
+            menu.Start();
+
+            // read the file 
+            // generate teacher list
+
+            // class teacherBiz (list teacher)
+            
+                // able to consult teacher
+                    // get all teachers
+                    // by id
+                    // by name - return list
+                    // by class - return list
+                    // by section - return list
+                // able to add teacher
+                // able to update teacher
+                // able to remove teacher
+
+            // update the file
+
+
+            
+
+
+        }
+    }
+}
