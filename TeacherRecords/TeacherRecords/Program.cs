eacd749a93933822735c3fddf33682db0c162a22
DeltaRig <hitomikaleiko@gmail.com>
2021-09-13 07:00:25 -0300

    creating project in VS

---
+++ TeacherRecords/TeacherRecords/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // read the file
+            // able to consult
+            // able to add
+            // able to edit
+            // update the file
+
+
+
+        }
+    }
+}
